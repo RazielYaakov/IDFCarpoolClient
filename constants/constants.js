c293b568554633e4e6d37d7833a1dc4d89764e9a
raziel <razielyaakov@gmail.com>
2020-04-03 18:15:18 +0300

    consts api

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,14 @@
 export const HOME_ROUTE_NAME = 'Home';
 export const TAB_NAVIGATOR_ROUTE_NAME = 'TabNavigator';
 export const PHONE_LOCAL_STORAGE_NAME = 'phone';
-
+export const SERVER_URL = 'https://idfcarpool.azurewebsites.net';
+export const LOGIN_API = 'login';
+export const UPDATE_API = 'updateuser';
+export const DELETE_API = 'deleteuser';
+export const FIND_RIDE_API = 'findride';
+export const SHOW_MY_RIDES_API = 'showmyriderequests';
+export const ACCEPT_RIDE = 'acceptride';
+export const CANCEL_RIDE = 'cancelride';
+export const SERVER_URL = 'phone';
 export const ALL_BASES = ["צריפין", "תל השומר", "קריה", "שלישות רמת גן", "עיר הבהדים"];
 export const ALL_CITIES = ["תל-אביב", "ירושלים", "נתניה", "חולון", "בת-ים", "אשקלון"];
